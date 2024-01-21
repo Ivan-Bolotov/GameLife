1b617389de2d4730cd72b5b7899e88ef5a2f9fbf
Ivan-Bolotov <ivanbolotov08@inbox.ru>
2024-01-22 00:52:11 +0300

    SetState handler has been added.

--- http/server/handler/handler.go
+++ http/server/handler/handler.go
@@ -3,11 +3,13 @@ package handler
 import (
 	"context"
 	"encoding/json"
+	"fmt"
+	"strconv"
 	"net/http"
+	"os"
 
 	"github.com/Ivan-Bolotov/game-life/internal/service"
 )
-
 
 // создадим новый тип для добавления middleware к обработчикам
 type Decorator func(http.Handler) http.Handler
@@ -26,6 +28,7 @@ func New(ctx context.Context,
 	}
 
 	serveMux.HandleFunc("/nextstate", lifeState.nextState)
+	serveMux.HandleFunc("/setstate", lifeState.setState)
 
 	return serveMux, nil
 }
@@ -46,5 +49,23 @@ func (ls *LifeStates) nextState(w http.ResponseWriter, r *http.Request) {
 	err := json.NewEncoder(w).Encode(worldState.Cells)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func (ls *LifeStates) setState(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		data := map[string]int{}
+		decoder := json.NewDecoder(r.Body)
+    	err := decoder.Decode(&data)
+		if err != nil {
+			fmt.Println(err.Error())
+		} else {
+			file, err := os.Create("state.cfg")
+			if err != nil {
+				fmt.Println(err.Error())
+			} else {
+				file.Write([]byte(strconv.Itoa(data["fill"]) + "%"))
+			}
+		}
 	}
 }
